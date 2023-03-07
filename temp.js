629b42380b0decc8e40b9d7d3f19f45af7ca6d4c
Mohammad Jubair <you@example.com>
2023-03-07 12:15:17 +0600

    Implement Push

---
+++ temp.js
@@ -0,0 +1,36 @@
+const API_Key = `8b05ed3770ffed2e7f05225afb4cfe9f`;
+
+const loadData = (city) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}&units=metric`
+  )
+    .then((res) => res.json())
+    .then((data) => displayTemprature(data));
+};
+
+const displayTemprature = (data) => {
+  setTheNewFunction("condition", data.weather[0].main);
+  setTheNewFunction("temp", data.main.temp);
+  console.log(data.weather[0].main);
+};
+
+const setTheNewFunction = (id, text) => {
+  let getFuncId = document.getElementById(id);
+  getFuncId.innerText = text;
+};
+
+document.getElementById("seacrh-btn").addEventListener("click", () => {
+  let getInputvalue = document.getElementById("input-value").value;
+  document.getElementById("city").innerText = getInputvalue;
+
+  loadData(getInputvalue);
+});
+loadData("Dhaka");
+
+// const myVar = prompt("Please Enter :");
+
+// if (isNaN(myVar)) {
+//   console.log("Input Valid ");
+// } else {
+//   console.log("not a valid");
+//}
